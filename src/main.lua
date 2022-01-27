9a7019503a549106511e26bbc99020143c24a203
Florian Rauls <frauls@web.de>
2022-01-27 20:05:18 +0100

    Update main.lua

---
+++ src/main.lua
@@ -0,0 +1,13 @@
+
+local sides = require("sides")
+local component = require("component")
+
+local transposer = component.transposer
+
+local testChest = sides.north
+
+for i=1; transposer.getInventorySize(testChest) do
+    -- if transposer.getStackInSlot(testChest, i)["name"] ~= "minecraft.air" do
+    local stack = transposer.getStackInSlot(testChest, i)
+    print("Slot: ", i, "Item: ", stack["name"], "Count: ", stack["count"])
+end
