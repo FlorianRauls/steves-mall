c4432a0c28cbe085e4266e5a7513157090025f67
Florian Rauls <frauls@web.de>
2022-01-28 23:43:32 +0100

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -174,7 +174,7 @@ end
 function buttons.fillTable()
     local i = 1
     for k, v in pairs(priceCatalogue) do
-        buttons.setTable(k, test1, 10,20*i,3,5)
+        buttons.setTable(k, test1, 10,20,3,5*i)
         i = i+1  
     end
     buttons.screen()
